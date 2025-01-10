ba8e9d9667246de3644dcdcfa16cdc9d478bf101
sheffykhlg <56907336+sheffykhlg@users.noreply.github.com>
2025-01-10 14:51:50 +0530

    Update config.py

--- config.py
+++ config.py
@@ -11,3 +11,4 @@ MONGO = getenv("MONGO", "mongodb+srv://mongodb7575:mkCNT8b2LZJX5ekf@cluster0.bcu
 OWNER_ID = int(getenv("OWNER_ID", 6514361814)) #Go to @ThunderrXbot and type /id and put that value here. 
 FSUB = bool(getenv("FSUB", True)) #Set this True if you want to enable force subscription from users else set to False.
 PICS = getenv('PICS', 'https://i.ibb.co/JHd49Yb/file-629.jpg').split()
+SESSION = environ.get("SESSION", "BQE6K2UAHvTKH1_6wr5tovh2AgrIEblRJTkreBFZyvxaow5WX_yf0LwcSRpbzC4WPcqRlAfnl4U1WZiV6IBtH20xlh1WLGFxWBEnIkyt1HrDA17ZAD--hl--ffqDNJsVgwkqics8OXrtiS1pIYk7HIUaJNbJyO16zPiGJnMoF5_3bN-MqjF45NoniSnxLQZ9NJqGNqainQ8bSyG0eszeooBeXBq2fzhwTGQlcltFvQWvePgBnuRPiLytbeeuDWHK2QZzp8aYU-goDoxn549Xx291l6etWfQHNJE41Vw3WOaOxX9crQPTUHBNp91-i_bG8TIzUrWtLuyY667alSzIrVQZ4E8RUQAAAAGESUXWAA") #pyrogram v2 user session ⚠️ v2 required ⚠️ & User must joined in your channel
