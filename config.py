83e7c28d18d21f7f48639b184b49f3a839aaa4fd
sheffykhlg <56907336+sheffykhlg@users.noreply.github.com>
2025-01-10 13:50:20 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,11 +2,11 @@ from os import getenv
 from dotenv import load_dotenv
 
 #Necessary Variables 
-API_ID = int(getenv("API_ID", 29400566))
-API_HASH = getenv("API_HASH", "8fd30dc496aea7c14cf675f59b74ec6f")
-BOT_TOKEN = getenv("BOT_TOKEN") #Put your bot token here
-CHANNEL = getenv("CHANNEL", "ZenBotX") #Your public channel username without @ for force subscription.
-MONGO = getenv("MONGO") #Put mongo db url here
+API_ID = int(getenv("API_ID", 20589413))
+API_HASH = getenv("API_HASH", "654d559a9a91daeecd9e760fc73e6766")
+BOT_TOKEN = getenv("BOT_TOKEN", "8041789409:AAFYu0Y1-08-BG9536ZZevLPhQDcgK4VtpM") #Put your bot token here
+CHANNEL = getenv("CHANNEL", "moviescrownvip1") #Your public channel username without @ for force subscription.
+MONGO = getenv("MONGO", "mongodb+srv://mongodb7575:mkCNT8b2LZJX5ekf@cluster0.bcuh8.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0") #Put mongo db url here
 #Optional Variables
-OWNER_ID = int(getenv("OWNER_ID", 6542673293)) #Go to @ThunderrXbot and type /id and put that value here. 
-FSUB = bool(getenv("FSUB", False)) #Set this True if you want to enable force subscription from users else set to False.
+OWNER_ID = int(getenv("OWNER_ID", 6514361814)) #Go to @ThunderrXbot and type /id and put that value here. 
+FSUB = bool(getenv("FSUB", True)) #Set this True if you want to enable force subscription from users else set to False.
